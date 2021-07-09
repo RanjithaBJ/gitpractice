639554f543e4002e209301bea1673fc50cd52f0b
Ranjitha <root@ip-172-31-47-89.us-east-2.compute.internal>
2021-07-09 16:04:27 +0000

    second round

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ gggggggSystem.out.printlnSystem.out.printlnSystem.out.printlnhhhclass Hello
 {
 public static void main(String args[])
 {
-System.out.println("hello");
+System.out.println("hi");
 }
 }
 
